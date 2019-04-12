32a0ee7e64adef3369593d4c6c965ddd49ba5bcf
Philippe Deslauriers <philippe.deslauriers@intact.net>
2019-04-12 15:12:13 -0400

    added extension

---
+++ com/myexample/my_service/request/package-info.java
@@ -0,0 +1,9 @@
+//
+// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.3.1 
+// Voir <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
+// Généré le : 2019.04.12 à 02:58:32 PM EDT 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.myexample.com/my-service/request")
+package com.myexample.my_service.request;
